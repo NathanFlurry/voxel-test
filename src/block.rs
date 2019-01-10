2caf5a2dc2361f7f249ab689a471428156bdd162
NathanFlurry <github@nathanflurry.com>
2019-01-09 19:17:00 -0700

    Fixed block indexes

--- src/block.rs
+++ src/block.rs
@@ -1,7 +1,7 @@
 pub struct BlockType(u8);
 
 impl BlockType {
-    pub fn is_air(&self) -> bool { self == 0 }
+    pub fn is_air(&self) -> bool { self.0 == 0 }
 
     pub fn is_transparent(&self) -> bool { self.is_air() }
 }
--- src/chunk.rs
+++ src/chunk.rs
@@ -1,25 +1,29 @@
 use crate::block::Block;
 
-pub struct ChunkIndex {
+pub struct BlockIndex {
     pub x: usize,
     pub y: usize,
     pub z: usize
 }
 
-impl ChunkIndex {
+impl BlockIndex {
     #[inline]
-    pub fn new(x: usize, y: usize, z: usize) -> ChunkIndex {
-        ChunkIndex { x, y, z }
+    pub fn new(x: usize, y: usize, z: usize) -> BlockIndex {
+        BlockIndex { x, y, z }
     }
 
-//    #[inline]
-//    fn from_data_index(usize) -> ChunkPosition {
-//
-//    }
+    #[inline]
+    fn from_data_index(i: usize) -> BlockIndex {
+        let z = i / (Chunk::SIZE_X * Chunk::SIZE_Y);
+        let y = (i - z * Chunk::SIZE_X * Chunk::SIZE_Y) / Chunk::SIZE_X;
+        let x = i - Chunk::SIZE_X * (y + Chunk::SIZE_Y * z);
+
+        BlockIndex::new(x, y, z)
+    }
 
     #[inline]
-    fn to_data_index(self) -> usize {
-        self.x + self.y * Chunk::HORIZONTAL_SIZE + self.z * Chunk::HORIZONTAL_SIZE * Chunk::VERTICAL_SIZE
+    fn get_data_index(&self) -> usize {
+        self.x + Chunk::SIZE_X * (self.y + Chunk::SIZE_Y * self.z)
     }
 }
 
@@ -28,31 +32,56 @@ pub struct Chunk {
 }
 
 impl Chunk {
-    const HORIZONTAL_SIZE: usize = 32;
-    const VERTICAL_SIZE: usize = 64;
-    const DATA_SIZE: usize = Chunk::HORIZONTAL_SIZE * Chunk::VERTICAL_SIZE;  // u16
+    const SIZE_X: usize = 32;
+    const SIZE_Y: usize = 64;
+    const SIZE_Z: usize = 32;
+    const DATA_SIZE: usize = Chunk::SIZE_X * Chunk::SIZE_Y * Chunk::SIZE_Z;  // 2^16
 
-    fn get_block(&self, position: ChunkIndex) -> &Block {
-        &self.data[position.to_data_index()]
+    fn get_block(&self, position: &BlockIndex) -> &Block {
+        &self.data[position.get_data_index()]
+    }
+
+    fn get_block_mut(&mut self, position: &BlockIndex) -> &mut Block {
+        &mut self.data[position.get_data_index()]
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::chunk::Chunk;
-    use crate::chunk::ChunkIndex;
+    use crate::chunk::BlockIndex;
 
     #[test]
-    fn data_index() {
+    fn get_data_index() {
         // Manual data index counter
         let mut i = 0;
 
         // Iterate each possible x, y, and z value and check it against the calculated data index
-        for z in 0..Chunk::HORIZONTAL_SIZE {
-            for y in 0..Chunk::VERTICAL_SIZE {
-                for x in 0..Chunk::HORIZONTAL_SIZE {
-                    let index = ChunkIndex::new(x, y, z);
-                    assert_eq!(index.to_data_index(), i);
+        for z in 0..Chunk::SIZE_Z {
+            for y in 0..Chunk::SIZE_Y {
+                for x in 0..Chunk::SIZE_X {
+                    let index = BlockIndex::new(x, y, z);
+                    assert_eq!(index.get_data_index(), i);
+                    i += 1;
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn from_data_index() {
+        // Manual data index counter
+        let mut i = 0;
+
+        // Iterate each possible x, y, and z value and check it against the calculated data index
+        for z in 0..Chunk::SIZE_Z {
+            for y in 0..Chunk::SIZE_Y {
+                for x in 0..Chunk::SIZE_X {
+                    let index = BlockIndex::from_data_index(i);
+                    println!("i {}", i);
+                    assert_eq!(index.x, x);
+                    assert_eq!(index.y, y);
+                    assert_eq!(index.z, z);
                     i += 1;
                 }
             }
